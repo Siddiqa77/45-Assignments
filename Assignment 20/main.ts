3da2a76fcc629faf7cfd52902a86b17f04ea7c99
Siddiqa Badar <Siddiqaasim778899@gmail.com>
2024-04-14 19:45:27 +0500

    Question 20

---
+++ Assignment 20/main.ts
@@ -0,0 +1,16 @@
+// Q no.21 They think of something you could store in a TypeScript Object.
+//  Write a program that creates Objects containing these items.
+
+interface student {
+    name: string;
+    age: number;
+    marks: number;
+}
+let student = { 
+    name : "Saba",
+    age : 23,
+    marks : 450
+};
+
+
+console.log(student);
